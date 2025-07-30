eb0e7c6acc73d408febbfa704dfcd6a3667c67f1
prasanna0493 <purambhavani96@gmail.com>
2025-07-30 13:45:11 +0530

    uploading program.py

---
+++ program.py
@@ -0,0 +1,13 @@
+name="MLWEC"
+number=10.0
+isOn=True
+
+
+#Arithmetic - +,-,*,/,%
+#Logical - and,or,not
+#Comparison - <,>,==,!=,<=,>=
+#Assignment - =,+=,-=,*=,/=
+if (number>5):
+    print(name)
+else:
+    print(isOn)
